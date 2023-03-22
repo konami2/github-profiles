076a3056552bf8a10476a53da46bfa47c2576411
aqiouami <abdelmottaliblol@gmail.cim>
2023-03-22 10:19:26 +0000

    first commit

--- app.js
+++ app.js
@@ -8,8 +8,58 @@ const numberRepos = document.getElementById('number-repos');
 const bio = document.getElementById('bio');
 const joined = document.querySelector('.date');
 const date = document.querySelector('#date');
+const country = document.getElementById('location');
 const login = document.getElementById('login');
+const locationIcon = document.querySelector('.fa-location-dot');
+const twitterIcon = document.querySelector('.fa-square-twitter');
+const githubLink = document.getElementById('github-link');
+const twitterr = document.querySelector('#twitter');
+const linkIcon = document.querySelector('.fa-paperclip');
+const link = document.getElementById('link');
+const githubIcon = document.querySelector('fa-github');
+const github = document.getElementById('github');
 const picture = document.getElementById('profile-pic');
+
+const getDate = (string)=> {
+	let index = string.indexOf('T');
+	let removedT = string.substring(0, index);
+	const date = new Date(removedT);
+	const options = { month: 'long', day: 'numeric', year: 'numeric' };
+	return date.toLocaleDateString('en-US', options);
+}
+
+const changeLocation = (location) => {
+	if (!location) {
+		country.innerHTML = 'Not available';
+		country.style.color = 'grey';
+		locationIcon.style.color = 'grey';
+	}
+	else {
+		country.innerHTML = location;
+	}
+}
+
+const changeTwitter = (twitter)=> {
+	if (!twitter) {
+		twitterr.innerHTML = 'Not available';
+		twitterr.style.color = 'grey';
+		twitterIcon.style.color = 'grey';
+	}
+	else {
+		country.innerHTML = twitter;
+	}
+}
+
+const changeLink = (blog)=> {
+	if (!blog) {
+		link.innerHTML = 'Not available';
+		link.style.color = 'grey';
+		linkIcon.style.color = 'grey';
+	}
+	else {
+		country.innerHTML = twitter;
+	}
+}
 
 const fetchdata = async () => {
 	const response = await fetch (url);
@@ -17,7 +67,7 @@ const fetchdata = async () => {
 	console.log(data);
 	picture.src = data.avatar_url
 	login.innerHTML = data.login;
-	date.innerHTML = 'joined ' + data.created_at.split('T15:59:05Z').join('')
+	date.innerHTML = 'joined ' + getDate(data.created_at);
 	if (data.bio == null) {
 		bio.innerHTML = 'this profile has no bio'
 	}
@@ -26,17 +76,22 @@ const fetchdata = async () => {
 	}
 	numberRepos.innerHTML = data.public_repos;
 	numberFollowrs.innerHTML = data.followers;
-	numberFollowing.innerHTML = data.following
-	hashtag.innerHTML = '@' + data.login
+	numberFollowing.innerHTML = data.following;
+	hashtag.innerHTML = '@' + data.login;
+	githubLink.setAttribute('href', data.html_url);
+	changeLocation(data.location);
+	changeTwitter(data.twitter_username);
+	changeLink(data.blog);
 }
 
 button.addEventListener('click', ()=> {
 	url = 'https://api.github.com/users/' + search.value;
 	fetchdata();
 })
-
-let flag = 1;
-if (flag) {
-	fetchdata();
-	flag = 0;
-}
+search.addEventListener('keypress', (event)=> {
+	if (event.key === "Enter" && search.value) {
+		url = 'https://api.github.com/users/' + search.value;
+		fetchdata();
+	}
+})
+fetchdata();
